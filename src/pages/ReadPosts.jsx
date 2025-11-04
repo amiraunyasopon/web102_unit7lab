701982431dfd0884df21e596385f5a1d9497497b
Amirsina A. Albekord <aalbekor@uci.edu>
2025-11-04 09:57:46 -0800

    Checkpoint 5

--- src/pages/ReadPosts.jsx
+++ src/pages/ReadPosts.jsx
@@ -1,31 +1,45 @@
 import { useState, useEffect } from 'react'
 import Card from '../components/Card'
+import { supabase } from '../client'
 
 const ReadPosts = (props) => {
 
     const [posts, setPosts] = useState([])
 
     useEffect(() => {
-        setPosts(props.data)
+        const fetchPost = async () => {
+            // specifying which table we want it from ('Posts')
+            // returns all database entries with .select()
+            // .order orders the fetched db entries by a given column(s)
+            // update the variable state of variable 'posts' by passing returned db entries to data in setPosts
+            const { data } = await supabase
+                .from('Posts')
+                .select()
+                .order('created_at', { ascending: true })
+
+            // set state of posts
+            setPosts(data)
+        }
+        fetchPost()
     }, [props])
-    
+
     return (
         <div className="ReadPosts">
             {
                 posts && posts.length > 0 ?
-                [...posts]
-                .sort((a, b) => a.id - b.id)
-                .map((post,index) => 
-                    <Card 
-                        key={post.id}
-                        id={post.id} 
-                        title={post.title}
-                        author={post.author}
-                        description={post.description}
-                    />
-                ) : <h2>{'No Challenges Yet 😞'}</h2>
+                    [...posts]
+                        .sort((a, b) => a.id - b.id)
+                        .map((post, index) =>
+                            <Card
+                                key={post.id}
+                                id={post.id}
+                                title={post.title}
+                                author={post.author}
+                                description={post.description}
+                            />
+                        ) : <h2>{'No Challenges Yet 😞'}</h2>
             }
-        </div>  
+        </div>
     )
 }
 
